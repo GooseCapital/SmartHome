8fbd7ae37295eaed714ea781428bba8906f82043
Tiến Dũng <diachiemail98@gmail.com>
2018-11-13 10:30:34 +0700

    Change facebook callback

--- model/auth.js
+++ model/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth': {
         'clientID': '176649286604155', // your App ID
         'clientSecret': '694ed5440bfb31a58ed5e0116e8511d8', // your App Secret
-        'callbackURL': 'https://localhost:3000/users/auth/facebook/callback',
+        'callbackURL': 'https://smarthome-nodejs.herokuapp.com/users/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields': ['id', 'email', 'name'] // For requesting permissions from Facebook API
     }
