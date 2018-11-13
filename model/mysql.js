f6c6fb5d5a8948d92710c7a2d161256f0325cdf5
Tiến Dũng <diachiemail98@gmail.com>
2018-11-13 09:46:14 +0700

    change database to heroku

--- model/mysql.js
+++ model/mysql.js
@@ -1,12 +1,20 @@
 var mysql = require('mysql');
 var util = require('util')
 
+// var pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'smarthome',
+//     multipleStatements: true
+// });
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'smarthome',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b74399cdef7719',
+    password: 'b8c5e2d6',
+    database: 'heroku_45ce012ae41eafd',
     multipleStatements: true
 });
 pool.getConnection((err, connection) => {
@@ -24,9 +32,9 @@ pool.getConnection((err, connection) => {
     if (connection) connection.release()
     return
 });
-pool.on('error', function(err) {
-    console.log("[mysql error]",err);
-  });
+pool.on('error', function (err) {
+    console.log("[mysql error]", err);
+});
 
 pool.query = util.promisify(pool.query) // Magic happens here.
 module.exports = pool;
@@ -36,4 +44,3 @@ module.exports.addFbUser = async (user) => {
     let query = `INSERT INTO users (email, fbid, image, fbName, role, timeJoin) VALUES ('${user.email}', ${user.fbid}, '${user.image}', '${user.fbName}', 1, ${Math.round(new Date().getTime()/1000)})`;
     await pool.query(query);
 };
-
